03c1e30fbc7860a53008cee532e6f1d75f817dd5
Łukasz Kaim <kaimukasz@gmail.com>
2023-01-31 20:42:58 +0100

    Add my first commit

---
+++ modul3.py
@@ -0,0 +1,16 @@
+# ZADANIE 1
+print("ZADANIE 1\n")
+shopping_list = {
+  "piekarnia" : ["chleb", "bułki", "pączek"],
+  "warzywniak" : ["marchew", "seler", "rukola"]
+}
+print("Lista zakupów")
+
+for shop in shopping_list:
+  for shops in range(len(shopping_list[shop])):
+    shopping_list[shop][shops] = shopping_list[shop][shops].capitalize()
+
+
+  print(f"Idę do {shop}, kupuję tu następujące rzeczy: {shopping_list[shop]}")
+  suma = len(shopping_list[shop][shops])
+print(f"W sumie kupuję {suma} produktów.")
